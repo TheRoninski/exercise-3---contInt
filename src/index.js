f615632198c8fc77c1531710ebdbb271b4c514d9
Ron <if22b080@technikum-wien.at>
2025-04-09 10:07:38 +0200

    implement library functions (index.js) and setup (package.json)

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function generateStrongPassword(length = 12) {
+    const allowedPasswordCharacters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    return Array
+        .from(
+            {length},
+            () => {
+                const randomAllowedPasswordCharacterIndex =
+                    Math.floor(Math.random() * allowedPasswordCharacters.length);
+                return allowedPasswordCharacters.charAt(randomAllowedPasswordCharacterIndex);
+            })
+        .join('');
+}
+
+function isStrongPassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    return /[A-Z]/.test(password) &&
+        /[a-z]/.test(password) &&
+        /\d/.test(password) &&
+        /[!@#$%^&*()_+]/.test(password);
+}
+
+module.exports = {
+    generateStrongPassword,
+    isStrongPassword
+}
