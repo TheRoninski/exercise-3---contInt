8e2c139b848a8595a893e3724a6739f04983c25e
Ron <if22b080@technikum-wien.at>
2025-04-09 11:03:31 +0200

    add test

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,11 +2,20 @@ const {generateStrongPassword, isStrongPassword} = require('../dist/index');
 
 describe('testing passwords', () => {
 
-    test('generateStrongPassword returns a strong password', () => {
+    test('generateStrongPassword returns a password with the given length', () => {
         const length = 12;
         const password = generateStrongPassword(length);
 
         expect(password.length).toBe(length);
+    })
+
+    test('generateStrongPassword returns a strong password', () => {
+        const length = 12;
+        const password = generateStrongPassword(length);
+        const isStrong = isStrongPassword(password);
+
+        expect(password.length).toBe(length);
+        expect(isStrong).toBeTruthy();
     })
 
     test('isStrongPassword returns false if password length is less than 8', () => {
